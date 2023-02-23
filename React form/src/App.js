2f2ebbf4490b43f4c9db85380829986c4e8d4534
dhineshnachi16 <121012951+dhineshnachi16@users.noreply.github.com>
2023-02-23 15:57:21 +0530

    Add files via upload

---
+++ React form/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App text-center">
+      <header className="App-header">
+      <h3>using the</h3>
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
+/*<p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>*/
